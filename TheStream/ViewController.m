76afdac9a01f52153ec931e29c58f1cd7cb1c301
Allen Huang <litleyelow@gmail.com>
2016-06-28 18:11:26 +0800

    inverse relationship demo

--- TheStream/ViewController.m
+++ TheStream/ViewController.m
@@ -8,6 +8,13 @@
 
 #import "ViewController.h"
 
+// utils
+#import <Realm/Realm.h>
+
+// models
+#import "RTArticleModel.h"
+#import "RTCategoryModel.h"
+
 @interface ViewController ()
 
 @end
@@ -17,11 +24,44 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+
+    [self setUpDataBase];
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+- (void)setUpDataBase
+{
+    RLMRealm *realm = [RLMRealm defaultRealm];
+    [realm beginWriteTransaction];
+    [realm deleteAllObjects];
+    [realm commitWriteTransaction];
+
+    [realm transactionWithBlock:^{
+        [RTArticleModel createInRealm:realm withValue:@[@"This article contains both finance and political content."]];
+        [RTArticleModel createInRealm:realm withValue:@[@"This article contains only finance content"]];
+
+        RTCategoryModel *financeCategory = [[RTCategoryModel alloc] init];
+        financeCategory.name = @"finance";
+        [financeCategory.articles addObjects:[RTArticleModel objectsWhere:@"title contains 'finance'"]];
+
+        RTCategoryModel *politicalCategory = [[RTCategoryModel alloc] init];
+        politicalCategory.name = @"political";
+        [politicalCategory.articles addObjects:[RTArticleModel objectsWhere:@"title contains 'political'"]];
+
+        [RTCategoryModel createInRealm:realm withValue:financeCategory];
+        [RTCategoryModel createInRealm:realm withValue:politicalCategory];
+    }];
+
+    // Log all articles and its categories using the "categories" inverse relationship
+    RLMResults *allArticles = [RTArticleModel allObjects];
+    for (RTArticleModel *article in allArticles) {
+        NSArray *categoriesName = [article.categories valueForKeyPath:@"name"];
+        NSLog(@"\n [Article] %@\n [Categories] %@\n", article.title, categoriesName);
+    }
 }
 
 @end
